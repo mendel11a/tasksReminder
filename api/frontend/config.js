852fc4cfb67e4f5d67dd20d3458b1b2c11f6dfc1
mendel11a <57116911+mendel11a@users.noreply.github.com>
2022-01-02 04:51:16 +0200

    Update config.js

--- api/frontend/config.js
+++ api/frontend/config.js
@@ -2,5 +2,5 @@ import axios from "axios"
 
 
 export const axiosInstance =axios.create({
-    baseURL:"https://tasks-reminders.herokuapp.com/api/",
+    baseURL:"https://tasks-reminders.herokuapp.com/",
 })
